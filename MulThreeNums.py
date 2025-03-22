b20198461694c947daf86f431bcf58b514d01d12
xiaofeijie <129645612+xiaofeijie@users.noreply.github.com>
2025-03-22 11:29:05 +0800

    Create MulThreeNums.py
    New changes are saved

---
+++ MulThreeNums.py
@@ -0,0 +1,10 @@
+def multiply_three_nums(num1, num2, num3):
+    return num1 * num2 * num3
+
+
+if __name__ == "__main__":
+    num1 = 2
+    num2 = 3
+    num3 = 4
+    result = multiply_three_nums(num1, num2, num3)
+    print(f"三个数字 {num1}、{num2} 和 {num3} 的乘积是: {result}")
